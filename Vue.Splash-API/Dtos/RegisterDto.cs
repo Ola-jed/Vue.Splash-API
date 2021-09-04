e6da0640ccad61ec0edf267c265ef12a63fb1f04
olabijed@gmail.com <olabijed@gmail.com>
2021-09-04 18:19:38 +0100

    White space removed

--- Vue.Splash-API/Dtos/RegisterDto.cs
+++ Vue.Splash-API/Dtos/RegisterDto.cs
@@ -6,11 +6,9 @@ namespace Vue.Splash_API.Dtos
     {
         [Required(ErrorMessage = "User Name is required")]
         public string Username { get; set; }
-
         [EmailAddress]
         [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
-
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
