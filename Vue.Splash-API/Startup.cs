2df2c973bee72e874d0f9c8d0fee490959af707c
olabijed@gmail.com <olabijed@gmail.com>
2021-09-17 16:11:46 +0100

    Split methods to extensions methods

--- Vue.Splash-API/Startup.cs
+++ Vue.Splash-API/Startup.cs
@@ -1,18 +1,12 @@
 using System;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using Vue.Splash_API.Data.Context;
 using Vue.Splash_API.Data.Repositories;
 using Vue.Splash_API.Extensions;
-using Vue.Splash_API.Models;
 using Vue.Splash_API.Services.Auth;
 using Vue.Splash_API.Services.Mail;
 using Vue.Splash_API.Services.Storage;
@@ -47,40 +41,10 @@ namespace Vue.Splash_API
             services.AddTransient<IMailService, MailService>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedEmail = true)
-                .AddEntityFrameworkStores<SplashContext>()
-                .AddDefaultTokenProviders();
+            services.ConfigureIdentity();
             services.AddMemoryCache();
-            services.AddCors(options =>
-            {
-                options.AddPolicy(name: OriginsAllowed,
-                    corsPolicyBuilder =>
-                    {
-                        corsPolicyBuilder
-                            .AllowAnyOrigin()
-                            .AllowAnyMethod()
-                            .AllowAnyHeader();
-                    });
-            });
-            services.AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(options =>
-                {
-                    options.SaveToken = true;
-                    options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidAudience = Configuration["JWT:ValidAudience"],
-                        ValidIssuer = Configuration["JWT:ValidIssuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
-                    };
-                });
+            services.ConfigureCors(OriginsAllowed);
+            services.ConfigureAuthentication(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -92,7 +56,6 @@ namespace Vue.Splash_API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vue.Splash_API v1"));
             }
-
             app.UseRouting();
             app.UseCors(OriginsAllowed);
             app.UseAuthentication();
